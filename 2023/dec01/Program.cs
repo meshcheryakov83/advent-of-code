2ecbfd936f3372422202cfbafae99f035e0e1869
Aleksey Meshcheryakov <meshcheryakov83@gmail.com>
2023-12-01 19:35:54 +0100

    small improvement

--- 2023/dec01/Program.cs
+++ 2023/dec01/Program.cs
@@ -1,26 +1,25 @@
 ﻿var digits = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-var reversedDigits = digits.Select(Reverse).ToArray();
+var reversed = digits.Select(Reverse).ToArray();
 
 var lines = File.ReadAllLines("in.txt");
-var sum = lines.Sum(x => FindFirst(x, digits) * 10 + FindFirst(Reverse(x), reversedDigits));
+
+var sum = lines.Sum(x => FindFirst(x, digits) * 10 + FindFirst(Reverse(x), reversed));
 Console.WriteLine(sum);
 
 static string Reverse(string s) => new(s.Reverse().ToArray());
 
-int FindFirst(string line, string[] pattens)
+static int FindFirst(string line, string[] pattens)
 {
+    var dict = pattens.Select((pattern, index) => (pattern, index))
+        .ToDictionary(x => x.pattern, x => x.index);
     for (int i = 0; i < line.Length; i++)
     {
         if (char.IsDigit(line[i]))
             return int.Parse(line[i].ToString());
-    
-        for (int num = 1; num < pattens.Length; num++)
-        {
-            var pattern = pattens[num];
-            if (line.Substring(i).StartsWith(pattern))
-                return num;
-        }
+
+        var val = dict.FirstOrDefault(x => line[i..].StartsWith(x.Key));
+        if (val.Key != null) return val.Value;
     }
-    
-    throw new Exception("No first");
+
+    throw new Exception("No any matches");
 }
