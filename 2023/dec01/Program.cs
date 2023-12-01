c1914af0c03b53eb349065d7af268fbefcadb5dd
Aleksey Meshcheryakov <meshcheryakov83@gmail.com>
2023-12-01 19:18:30 +0100

    2023 dec 01

---
+++ 2023/dec01/Program.cs
@@ -0,0 +1,26 @@
+﻿var digits = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+var reversedDigits = digits.Select(Reverse).ToArray();
+
+var lines = File.ReadAllLines("in.txt");
+var sum = lines.Sum(x => FindFirst(x, digits) * 10 + FindFirst(Reverse(x), reversedDigits));
+Console.WriteLine(sum);
+
+static string Reverse(string s) => new(s.Reverse().ToArray());
+
+int FindFirst(string line, string[] pattens)
+{
+    for (int i = 0; i < line.Length; i++)
+    {
+        if (char.IsDigit(line[i]))
+            return int.Parse(line[i].ToString());
+    
+        for (int num = 1; num < pattens.Length; num++)
+        {
+            var pattern = pattens[num];
+            if (line.Substring(i).StartsWith(pattern))
+                return num;
+        }
+    }
+    
+    throw new Exception("No first");
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# [Advent Of Code](https://adventofcode.com/)
+
+- [2023](https://adventofcode.com/2023)
