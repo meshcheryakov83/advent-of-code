7e992673721392c4f5aaa3ead49462394b15f0c0
Aleksey Meshcheryakov <meshcheryakov83@gmail.com>
2023-12-02 10:06:20 +0100

    small improvement

--- 2023/dec02/Program.cs
+++ 2023/dec02/Program.cs
@@ -1,4 +1,6 @@
-﻿var games = File.ReadAllLines("in.txt")
+﻿var RED = "red"; var GREEN = "green"; var BLUE = "blue";
+
+var games = File.ReadAllLines("in.txt")
     .Select(x =>
     {
         var parts = x.Split(":");
@@ -13,7 +15,7 @@
                 return (color, count);
             }).ToArray();
 
-            var gameSet = new Dictionary<string, int> { ["red"] = 0, ["green"] = 0, ["blue"] = 0 };
+            var gameSet = new Dictionary<string, int> { [RED] = 0, [GREEN] = 0, [BLUE] = 0 };
             foreach (var gameSetPart in gameSetParts)
             {
                 gameSet[gameSetPart.color] = gameSetPart.count;
@@ -26,14 +28,14 @@
 
 var part1 = games.Sum(x =>
 {
-    return x.gameSets.Any(x => x["red"] > 12 || x["blue"] > 14 || x["green"] > 13) ? 0 : x.gameNumber;
+    return x.gameSets.Any(x => x[RED] > 12 || x[BLUE] > 14 || x[GREEN] > 13) ? 0 : x.gameNumber;
 });
 
 var part2 = games.Sum(x =>
 {
-    var green = Math.Max(x.gameSets.Max(x => x["green"]), 1);
-    var blue = Math.Max(x.gameSets.Max(x => x["blue"]), 1);
-    var red = Math.Max(x.gameSets.Max(x => x["red"]), 1);
+    var green = Math.Max(x.gameSets.Max(x => x[GREEN]), 1);
+    var blue = Math.Max(x.gameSets.Max(x => x[BLUE]), 1);
+    var red = Math.Max(x.gameSets.Max(x => x[RED]), 1);
     return red * green * blue;
 });
 
