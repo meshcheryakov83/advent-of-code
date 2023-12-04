b7905dc276e257df53da88e68de387c83a6ae2b2
Aleksey Meshcheryakov <meshcheryakov83@gmail.com>
2023-12-04 15:52:31 +0100

    small improvements

--- 2023/dec04/Program.cs
+++ 2023/dec04/Program.cs
@@ -1,23 +1,21 @@
 ﻿var wins = File.ReadAllLines("in.txt")
     .Select(x =>
     {
+        int[] parseNumbers(string str) => str.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
         var parts = x.Split(":|".ToCharArray());
-        var targetNumbers = parts[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
-        var numbers = parts[2].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        var targetNumbers = parseNumbers(parts[1]);
+        var numbers = parseNumbers(parts[2]);
         var winNumber = numbers.Intersect(targetNumbers).Count();
         return winNumber;
     }).ToArray();
 
-var part1 = wins.Sum(w => w > 0 ? Math.Pow(2, w - 1) : 0);
+var part1 = wins.Where(w => w > 0).Sum(w => Math.Pow(2, w - 1));
 Console.WriteLine("part 1: " + part1);
 
 var cardsCount = new List<int>();
-for(int ind = 0; ind < wins.Length; ind++)
-{ 
-    cardsCount.Add(1 + cardsCount
-        .Select((count, i) => ind - i <= wins[i] ? count : 0)
-        .Sum());
+for (int ind = 0; ind < wins.Length; ind++)
+{
+    cardsCount.Add(1 + cardsCount.Where((_, i) => ind - i <= wins[i]).Sum());
 }
-
 var part2 = cardsCount.Sum();
 Console.WriteLine("part 2: " + part2);
