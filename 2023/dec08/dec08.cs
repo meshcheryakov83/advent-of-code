1bed28eeaf57d95adfa90de8c08fb3d9e3733f08
Aleksey Meshcheryakov <meshcheryakov83@gmail.com>
2023-12-08 12:39:07 +0100

    dec 08

---
+++ 2023/dec08/dec08.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+
+var lines = File.ReadAllLines("in.txt");
+var instructions = lines[0];
+var map = lines[2..].Select(x =>
+{
+    var parts = x.Split(" =(),".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+    return new[] { parts[0], parts[1], parts[2] };
+}).ToDictionary(x => x[0]);
+
+var part1 = Part1();
+var part2 = Part2();
+Debug.Assert($"{part1} {part2}" == File.ReadAllText("expected_out.txt").Trim());
+Console.WriteLine("part1: " + part1);
+Console.WriteLine("part2: " + part2);
+
+long Part1()
+{
+    var cur = map["AAA"][0];
+    var instrIndex = 0;
+
+    while (cur != "ZZZ")
+    {
+        var instruction = instructions[instrIndex % instructions.Length];
+        cur = instruction == 'L' ? map[cur][1] : map[cur][2];
+        instrIndex++;
+    }
+
+    return instrIndex;
+}
+
+long Part2()
+{
+    var cur = map.Where(x => x.Key.EndsWith('A')).Select(x => x.Key).ToList();
+    var instrIndex = 0;
+    var steps = new List<int>();
+    while (cur.Any())
+    {
+        var instruction = instructions[instrIndex % instructions.Length];
+        for (int i = cur.Count-1; i >= 0; i--)
+        {
+            cur[i] = instruction == 'L' ? map[cur[i]][1] : map[cur[i]][2];
+            if (cur[i].EndsWith('Z'))
+            {
+                steps.Add(instrIndex + 1);
+                cur.RemoveAt(i);
+            }
+        }
+
+        instrIndex++;
+    }
+
+    return steps.
+        Select(x => ToPrimes(x))
+        .SelectMany(x => x.GroupBy(x => x))
+        .GroupBy(x=>x.Key)
+        .Select(x=>(x.Key, x.MaxBy(y=>y.Count()).Count()))
+        .Aggregate((long)1, (i, tuple) => i*(long)Math.Pow(tuple.Key, tuple.Item2));
+}
+
+IEnumerable<long> ToPrimes(long n)
+{
+    var i = 2;
+
+    while (n > 1)
+    {
+        if (n % i == 0)
+        {
+            n /= i;
+            yield return i;
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
